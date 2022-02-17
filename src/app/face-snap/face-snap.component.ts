a928427cf53191fe3c538db7ac4147e48cd052f7
JustinLafay <justin.lafay@insa-lyon.fr>
2022-02-17 21:00:08 +0100

    2 files modified, unsnap added

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,12 +11,26 @@ export class FaceSnapComponent {
   createdDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  snapText!: string;
 
   ngOnInit() {
     this.title = 'Princesse Mononoke';
     this.description = "Un belle photo de l'affiche du film";
     this.createdDate = new Date();
     this.snaps = 15;
-    this.imageUrl = 'https://fr.web.img2.acsta.net/pictures/21/02/09/12/46/1884055.jpg';
+    this.imageUrl =
+      'https://fr.web.img2.acsta.net/pictures/21/02/09/12/46/1884055.jpg';
+    this.snapText = 'Oh, snap !!';
+  }
+
+  onAddSnap() {
+    if (this.snapText == 'Oh, snap !!') {
+      this.snaps++;
+      this.snapText = 'Oops! Unsnap...';
+    }
+    else if (this.snapText == 'Oops! Unsnap...') {
+      this.snaps--;
+      this.snapText = 'Oh, snap !!';
+    }
   }
 }
